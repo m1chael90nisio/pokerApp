ee071dab5b977dba25bbd50149e6a48372141317
m1chael90nisio <m.nisiotis90@gmail.com>
2018-06-26 18:24:13 +0300

    changes

--- pairs.js
+++ pairs.js
@@ -1,7 +1,7 @@
 // Load the full build.
 import _ from 'lodash';
 
-      var arr=[{Value:'10',Suit:'Hearts'},{Value:'A',Suit:'Hearts'},{Value:'10',Suit:'Hearts'},{Value:'12',Suit:'Hearts'},{Value:'4',Suit:'Clubs'}];
+      var arr=[{Value:'10',Suit:'Hearts'},{Value:'10',Suit:'Hearts'},{Value:'10',Suit:'Hearts'},{Value:'A',Suit:'Hearts'},{Value:'A',Suit:'Clubs'}];
       var order = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
 
@@ -31,54 +31,17 @@ import _ from 'lodash';
       var filteredArray = _.uniqBy(sorted_arr,function(x){ return x.Value  ;});
       //console.log("\nUNIQUE VALUES\n");
       console.log(filteredArray);
+      var max=_.maxBy(filteredArray, function(o) { return o.cnt; });
+      //console.log(max.cnt);
+
 
       if(filteredArray.length==4 )
       console.log("exo 1 pair");
-      if(filteredArray.length==3)
-      console.log("exo three of a kind");
-      if(filteredArray.length==2 && !(filteredArray[0].cnt==3 ||filteredArray[1].cnt==2) )
+      if(filteredArray.length==3 && max.cnt==2)
+      console.log("exo 2 pairs");
+      if(filteredArray.length==3 && max.cnt==3 )
+      console.log("three of a kind")
+      if(filteredArray.length==2 && max.cnt==3 )
       console.log("three of a kind and a pair")
-      if(filteredArray.length==2 && !(filteredArray[0].cnt==4 ||filteredArray[1].cnt==4) )
-      console.log("2 pairs");
-      if(filteredArray.length==2 && (filteredArray[0].cnt==4 ||filteredArray[1].cnt==4))
+      if(filteredArray.length==2 && max.cnt==4 )
       console.log("four of kind");
-
-
-
-
-
-/*
-
-          console.log(filteredArray);
-          console.log("look");
-          var max=_.maxBy(filteredArray, function(o) { return o.cnt; });
-          console.log(max.cnt);
-
-
-
-        Array.prototype.straight_flush= function(){
-
-        var sorted = filteredArray;
-        var flag=true;
-
-
-        for (var i = 0; i < sorted.length; i++) {
-
-        if (!order.includes(sorted[i].Value) || sorted[i].cnt>1)
-        {
-        //console.log(sorted[i].cnt);
-        flag=false;
-        }
-        //console.log(order.includes(sorted[i].key));
-
-        }
-        if (flag==false){
-        console.log("no straight");
-        }
-        else {
-        console.log("pairs found");
-        }
-      }
-
-        var kati= filteredArray.straight_flush();
-*/
